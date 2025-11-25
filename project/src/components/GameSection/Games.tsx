614486b1616db89117b192b2a634a058cd1d408e
Gigi Nakhutsrishvili <nakhutsrishviligigi@gmail.com>
2025-11-26 03:11:10 +0400

    Added game selection page, font, hovers

--- project/src/components/GameSection/Games.tsx
+++ project/src/components/GameSection/Games.tsx
@@ -1,12 +1,40 @@
 import Banner from '../../assets/GTA 6 banner.svg'
+import game1 from '../../assets/GTA 5.svg'
+import game2 from '../../assets/GTA online.svg'
+import game3 from '../../assets/RED.svg'
+import game4 from '../../assets/RED Online.svg'
+import style from '../../button.module.css'
+
+type availableGames = {
+  game1: string
+  game2: string
+  game3: string
+  game4: string
+
+  
+}
 
 const Games = () => {
   return (
-    <div>
-        <div
-  className="w-full h-[850px] bg-cover bg-center bg-no-repeat"
-  style={{ backgroundImage: `url(${Banner})` }}
-></div>
+    <div className='flex items-center justify-center flex-col gap-20'>
+        <div className="w-full h-[850px] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${Banner})` }}></div>
+        <div className='w-[1160px] h-[555px] flex flex-col justify-between items-center'>
+                 <div className='flex w-full h-auto justify-between items-center'>
+                  <div className='h-auto w-auto font-extrabold text-[48px] pb-0.5'>Jogos em Destaque</div>
+                  <div className='flex w-[404px] h-8 justify-between items-center text-center'>
+                    <div className='h-8 w-[70px] bg-[#606060] rounded-[100px] pt-1 hover:scale-105 transition-transform duration-300 cursor-pointer'>PC</div>
+                    <div className='h-8 w-[106px] bg-[#606060] rounded-[100px] pt-1 hover:scale-105 transition-transform duration-300 cursor-pointer'>Console</div>
+                    <div className='h-8 w-[196px] bg-[#606060] rounded-[100px] pt-1 hover:scale-105 transition-transform duration-300 cursor-pointer'>Dispositivos Portáteis</div>
+                  </div>
+                 </div>
+            <div className='flex w-full justify-between'>
+              <img src={game1} alt="GTA 5" className="w-[260px] h-[315.85px] rounded-2xl hover:scale-105 transition-transform duration-300 cursor-pointer"/>
+              <img src={game2} alt="GTA 5 ONLINE" className="w-[260px] h-[315.85px] rounded-2xl hover:scale-105 transition-transform duration-300 cursor-pointer"/>
+              <img src={game3} alt="RED DEAD REDEMPTION"className="w-[260px] h-[315.85px] rounded-2xl hover:scale-105 transition-transform duration-300 cursor-pointer" />
+              <img src={game4} alt="RED DEAD REDEMPTION ONLINE" className="w-[260px] h-[315.85px] rounded-2xl hover:scale-105 transition-transform duration-300 cursor-pointer"/>
+            </div>
+            <button className={style.button1}>Ver todos os jogos</button>
+        </div>
 
     </div>
   )
--- project/src/components/Header/Header.tsx
+++ project/src/components/Header/Header.tsx
@@ -6,23 +6,25 @@ import Icon from '../../assets/UserIcon.svg'
 
 const Header = () => {
   return (
-     <div className='w-full bg-[#020202] h-[112px] flex items-center justify-between pl-[140px] pr-[140px]'>
+     <div className='h-[110px] w-full'>
+     <div className='w-full bg-[#020202] h-28 flex items-center justify-between pl-[140px] pr-[140px] fixed top-0'>
         <div className='h-auto flex w-[300px] items-center justify-between'>
          <img src={Logo} alt="" />
            <div className='flex items-center'>
-            <div className='text-white'>Jogos</div>
-            <img src={Arrow} alt="Arrow" />
+            <div className='hover:scale-105 transition-transform duration-300 cursor-pointer hover:text-amber-400'>Jogos</div>
+            <img src={Arrow} alt="Arrow"  className='hover:scale-105 transition-transform duration-300 cursor-pointer'/>
          </div>
-         <div className='text-white'>Loja</div>
-            <div className='text-white'>Ajuda</div>
+         <div className='hover:scale-105 transition-transform duration-300 cursor-pointer hover:text-amber-300'>Loja</div>
+            <div className='hover:scale-105 transition-transform duration-300 cursor-pointer hover:text-amber-300'>Ajuda</div>
         </div>
         <div className='h-auto w-[345px] flex items-center justify-between'>
-          <button className={'{style.button}'}>Baixe o Launcher</button>
+          <button className={style.button}>Baixe o Launcher</button>
           <div className='flex justify-between w-[118px]'>
-          <img src={SearchBar} alt="Search Bar" className='pl-1' />
-          <img src={Icon} alt="User Icon" />
+          <img src={SearchBar} alt="Search Bar" className='pl-1 hover:scale-105 transition-transform duration-300 cursor-pointer' />
+          <img src={Icon} alt="User Icon" className='hover:scale-105 transition-transform duration-300 cursor-pointer' />
           </div>
         </div>
+     </div>
      </div>
   )
 }
